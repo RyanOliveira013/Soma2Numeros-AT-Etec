daf9a2e9c3b30d5362328723f00e09579ca55e8c
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-09-25 20:33:55 -0300

    Soma de 2 Numeros

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Soma2Numeros_AT_Etec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int PrimeiroNumero;
+            int SegundoNumero;
+            int Resultado;
+
+            Console.Write("1o. numero: ");
+            PrimeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2o. numero: ");
+            SegundoNumero = Convert.ToInt32(Console.ReadLine());
+            
+            Resultado = PrimeiroNumero + SegundoNumero;
+
+            Console.WriteLine($"O Resultado e {Resultado} ");
+        }
+    }
+}
